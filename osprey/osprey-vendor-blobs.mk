513bb72d399fac28cbb89e61ece1130596c5420a
nickd81 <nickdoh81@gmail.com>
2016-12-14 20:24:10 -0500

    Revert "Add libqsap_sdk.so and libbt-vendor.so"
    This reverts commit 2f42fa6784e2ac724433237b1a3f07ee69553a1a.
    
    Change-Id: I65e1a31144935e19bd06d76ab3d3c0b8611d4da8

--- osprey/osprey-vendor-blobs.mk
+++ osprey/osprey-vendor-blobs.mk
@@ -128,5 +128,4 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/osprey/proprietary/vendor/lib/libmmqjpeg_codec.so:system/vendor/lib/libmmqjpeg_codec.so \
     vendor/motorola/osprey/proprietary/vendor/lib/liboemcamera.so:system/vendor/lib/liboemcamera.so \
     vendor/motorola/osprey/proprietary/vendor/lib/libqomx_jpegdec.so:system/vendor/lib/libqomx_jpegdec.so \
-    vendor/motorola/osprey/proprietary/vendor/lib/libqomx_jpegenc.so:system/vendor/lib/libqomx_jpegenc.so \
-    vendor/motorola/osprey/proprietary/lib/libqsap_sdk.so:system/lib/libqsap_sdk.so
+    vendor/motorola/osprey/proprietary/vendor/lib/libqomx_jpegenc.so:system/vendor/lib/libqomx_jpegenc.so
