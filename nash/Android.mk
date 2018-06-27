7a8352d135b7cc3ff610e5ac0f17e2b389de97dd
Josh Correll <jbats@dirtyunicorns.com>
2018-06-27 12:04:41 -0600

    nash: use platform certificates for moto mods
    Change-Id: Iba11772b213f2802635433e361fb86025dc703a7

--- nash/Android.mk
+++ nash/Android.mk
@@ -245,7 +245,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := ModService
 LOCAL_MODULE_OWNER := motorola
 LOCAL_SRC_FILES := proprietary/priv-app/ModService/ModService.apk
-LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
 LOCAL_DEX_PREOPT := false
@@ -257,7 +257,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := ProjectorApp
 LOCAL_MODULE_OWNER := motorola
 LOCAL_SRC_FILES := proprietary/priv-app/ProjectorApp/ProjectorApp.apk
-LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
 LOCAL_DEX_PREOPT := false
